01d44f2585439b41fa2a6f070a0ea4e384e1f7ad
garcialmg <leandromgarcia@gmail.com>
2025-10-23 15:58:19 -0300

    Projeto completo Aula 4 - Oferta 9

---
+++ RotativoO9/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RotativoO9.Entities;
+
+namespace RotativoO9.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cadastro> Cadastros { get; set; }
+        public DbSet<FilhoCadastro> FilhosCadastro { get; set; }
+
+    }
+}
---
+++ RotativoO9/WorkInProgress/WorkInProgress.cs
@@ -0,0 +1,32 @@
+﻿using RotativoO9.Data;
+
+namespace RotativoO9.WorkInProgress
+{
+    public interface IWorkInProgress
+    {
+        void BeginTransaction();
+        void Commit();
+        void Rollback(Exception ex);    
+    }
+    public class WorkInProgress : IWorkInProgress
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public WorkInProgress(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void BeginTransaction()
+        {
+        }
+
+        public void Commit()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void Rollback(Exception ex)
+        {
+        }
+    }
+}
